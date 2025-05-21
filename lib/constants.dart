66458375e20674a6b3cdf58fad0ccbdeddc42b19
Khaiqol <Khaiqol@gmail.com>
2025-05-21 20:47:20 +0700

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// colors that we use in our app
+const kPrimaryColor = Color(0xFF0C9869);
+const kTextColor = Color(0xFF0C9869);
+const kBackgroundColor = Color(0xFF0C9869);
+
+const double kDefaultPadding = 20.0;
