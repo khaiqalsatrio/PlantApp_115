f2ca958ebdc747f88178c6cb1db4bb4c271adda1
Khaiqol <Khaiqol@gmail.com>
2025-05-22 23:39:24 +0700

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app_115/constants.dart';
-import 'package:plant_app_115/screens/home/home_screen.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,11 +16,11 @@ class MyApp extends StatelessWidget {
       title: 'Plant App',
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
-        primaryColor: kPrimaryColor,
+        primaryColor: const Color.fromARGB(255, 231, 235, 231),
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
